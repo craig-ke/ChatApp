7efa732f8cb00d34a4fdcda6c91ff19a92d4cbf1
craig-ke <lawrence.olonyi.67@gmail.com>
2021-02-03 11:07:56 +0300

    add files

--- app/src/main/java/com/craigke/letschat/MainActivity.java
+++ app/src/main/java/com/craigke/letschat/MainActivity.java
@@ -1,21 +1,89 @@
 package com.craigke.letschat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
+import com.craigke.letschat.fragments.ChatsFragment;
+import com.craigke.letschat.fragments.UsersFragment;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar= findViewById(R.id.toolbar);
+//        setSupportActionBar(Toolbar);
+//        getSupportActionBar().setTitle("");
+
+        profile_image=findViewById(R.id.profile_image);
+        username=findViewById(R.id.username);
+
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener(){
+//set name and profile picture 49 to line 55
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User user = snapshot.getValue(User.class);
+//                username.setText(user.getUsername());
+//                if (user.getImageURL().equals("default")){
+//                    profile_image.setImageResource(R.mipmap.ic_launcher);
+//                }else{
+//                    Glide.with(MainActivity.this).load(user.getImageURL()).into(profile_image);
+//                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        TabLayout tabLayout= findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment( new ChatsFragment(),"chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"users");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,5 +108,36 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-}
+    static class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+           this.titles= new ArrayList<String>();
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
 }
